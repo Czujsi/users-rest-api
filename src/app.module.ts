d98dec4b39b2ccd79b0d246dc87d21544fecffe7
Czujsi <oskarbanaszczuk@gmail.com>
2024-10-31 14:09:02 +0100

    working database :D

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule, MyLoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
